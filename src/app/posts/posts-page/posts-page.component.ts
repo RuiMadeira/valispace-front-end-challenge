5ac9568ea8370cf220286c938e5ae7bff4e2dc90
RuiMadeira <ruinunomadeira@gmail.com>
2020-06-05 00:36:27 +0100

    Started working on the posts page component

--- src/app/posts/posts-page/posts-page.component.ts
+++ src/app/posts/posts-page/posts-page.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee';
+import { Post } from 'src/app/models/post';
+import { ManageEmployeeService } from '../../services/manage-employee/manage-employee.service';
+import { ManagePostService } from '../../services/manage-post/manage-post.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+const TEXT_PIECES: string[] = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Proin magna metus, venenatis sed sem id, dignissim pulvinar neque. Ut sed velit in massa fringilla iaculis.',
+  'Morbi tincidunt imperdiet ex sed lacinia. Cras augue ligula, vulputate sed tincidunt rutrum, sagittis non neque.',
+  'Donec pellentesque aliquam est, viverra imperdiet dui semper quis. Sed eu ipsum eget ex hendrerit blandit sed nec neque. Donec eget libero eros.'
+];
 
 @Component({
   selector: 'app-posts-page',
@@ -6,10 +18,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-page.component.css']
 })
 export class PostsPageComponent implements OnInit {
+  postsList: Array<Post>;
+  postSelected: Post;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private managePostService: ManagePostService, private manageEmployeeService: ManageEmployeeService,
+              private snackBar: MatSnackBar) {
+    // Create 100 post
+    const posts = Array.from({length: 100}, (_, k) => createNewPost(k + 1));
+    this.postsList = posts;
   }
 
+  ngOnInit(): void {
+    // this.postsList = this.managePostService.getPostList();
+  }
+
+  public selectEmployeeForEdit(post: Post): void {
+    this.postSelected = { ...post };
+  }
+
+  public newEmployee(): void {
+    this.postSelected = { id: undefined, employeeId: undefined, text: undefined, created: undefined, edited: undefined };
+  }
+
+  public addEmployee(): void {
+    this.postActionBehaviour(this.managePostService.createPost(this.postSelected),
+      'Employee added successfully',  'Error adding employee');
+  }
+
+  public editSelectedEmployee(): void {
+    this.postActionBehaviour(this.managePostService.editPost(this.postSelected),
+      'Employee edited successfully',  'Error editing employee');
+  }
+
+  public cancelEditing(): void {
+    this.postSelected = undefined;
+  }
+
+  private postActionBehaviour(condition: boolean, messageSuccess: string, messageFailure: string): void {
+    if (condition) {
+      this.snackBar.open(messageSuccess, 'Close', { duration: 3000 });
+      this.postSelected = undefined;
+    } else {
+      this.snackBar.open(messageFailure, 'Close', { duration: 3000 });
+    }
+  }
+}
+
+function randomDate(start: Date, end: Date) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+function createNewPost(id: number): Post {
+  const text = TEXT_PIECES[Math.round(Math.random() * (TEXT_PIECES.length - 1))]
+  + TEXT_PIECES[Math.round(Math.random() * (TEXT_PIECES.length - 1))]
+  + TEXT_PIECES[Math.round(Math.random() * (TEXT_PIECES.length - 1))];
+  const created = randomDate(new Date(2019, 0, 1), new Date());
+  const edited = randomDate(new Date(2020, 6, 1), new Date());
+
+  return { id, employeeId: id,  text, created, edited };
 }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsPageComponent } from './posts-page/posts-page.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [PostsPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
   ]
 })
 export class PostsModule { }
